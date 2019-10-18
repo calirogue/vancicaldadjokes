adf4277b5080bb5ba9f9ea3535d8f99e458b162a
Rogue Bell <mattbellantoni3@gmail.com>
2019-10-19 01:26:03 +0200

    added search

---
+++ components/SearchJokes.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="text" placeholder="search jokes">
+    <input type="submit" value="search jokes">
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SearchJokes',
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('search-text', this.text);
+      this.text='';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <SearchJokes v-on:search-text="searchText" />
     <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
   </div>
 </template>
@@ -7,9 +8,11 @@
 <script>
 import axios from 'axios';
 import Joke from '../../components/Joke';
+import SearchJokes from '../../components/SearchJokes';
 export default {
   components: {
-    Joke
+    Joke,
+    SearchJokes
   },
   data() {
     return {
@@ -31,6 +34,23 @@ export default {
       console.log(err)
     }
   
+  },
+  methods: {
+    async searchText(text) {
+      const config = {
+        headers: {
+          'Accept': 'application/json'
+        }
+      }
+
+      try {
+        const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config);
+
+        this.jokes = res.data.results;
+      } catch (err) {
+        console.log(err)
+      }
+    }
   },
   head() {
     return {
