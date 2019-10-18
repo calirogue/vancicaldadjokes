191d5e20c97d74a98a08b2208c9fed62492918da
Rogue Bell <mattbellantoni3@gmail.com>
2019-10-18 13:27:50 +0200

    vance jokes

---
+++ pages/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>About Vance Jokes</h1>
+    <p>App that displays corny jokes.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Example",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Horny Vance jokes"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
